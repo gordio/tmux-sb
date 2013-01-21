d0164466880e587b551b35b6b21298574d20bb12
Oleg Gordienko <admin@gordio.pp.ua>
2013-01-21 01:18:34 +0000

    Many updates and fix /0 (if not exist swap)

--- Makefile
+++ Makefile
@@ -1,42 +1,45 @@
 #!/usr/bin/make -f
 
-PROJNAME := $(notdir $(PWD))
-CFILES := $(wildcard src/*.c)
-OBJECTS = $(CFILES:.c=.o)
-PREFIX = /usr/local
+#PROJNAME := $(notdir $(PWD))
+PROJNAME := "tmux-sb"
+CFILES   := $(wildcard src/*.c)
+OBJECTS  := $(CFILES:.c=.o)
+PREFIX   ?= "/usr/local"
 
-PCFLAGS = -O2
-PCFLAGS += $(CFLAGS)
+_ver0   := $(shell git describe HEAD 2>/dev/null)
+_ver1   := $(shell git describe --tags HEAD 2>/dev/null)
+_ver2   := $(shell git rev-parse --verify --short HEAD 2>/dev/null)
+VERSION := $(or $(_ver0),$(_ver1),$(_ver2))
 
-.SUFFIXES: .a .c .h .o
-.PHONY: all static clear clean install uninstall types
+CFLAGS += -O2 -std=c99
+CFLAGS += -Isrc
+CFLAGS += -g -Wall -Wextra -pedantic
+CFLAGS += -DVERSION=\"$(VERSION)\"
 
-all: $(OBJECTS)
-	@echo -e "\033[1;32m LINK\033[0m" $(PROJNAME)
-	@$(CC) $(OBJECTS) -o "$(PROJNAME)"
 
-%.o: %.c
-	@echo -e "\033[1m   CC\033[0m" $<
-	@$(CC) $(PCFLAGS) -c $< -o $@
+.SUFFIXES: .c .h .o
+.PHONY: all clear clean install uninstall types
+
+all: $(PROJNAME)
+
+$(PROJNAME): $(OBJECTS)
+	@echo -e "\033[1;32m LINK\033[0m" ${PROJNAME}
+	@$(CC) -o ${PROJNAME} $(OBJECTS)
+
+%.o: %.c %.h
+	@echo -e "\033[1m   CC\033[0m $< -> $@"
+	@$(CC) $(CFLAGS) -c -o $@ $<
 
 clear:
 	@echo -e "\033[1;31m   RM\033[0m" $(OBJECTS)
 	@rm -f $(OBJECTS)
 
 clean: clear
-	@echo -e "\033[1;31m   RM\033[0m" "$(PROJNAME)"
-	@rm -f "$(PROJNAME)"
+	@echo -e "\033[1;31m   RM\033[0m" "${PROJNAME}"
+	@rm -f ${PROJNAME}
 
 install: all
-	install -m 744 "$(PROJNAME)" "$(PREFIX)/bin/"
+	install -m 755 ${PROJNAME} "${PREFIX}/bin/"
 
 uninstall: all
 	rm "$(PREFIX)/bin/$(PROJNAME)"
-
-types: types.vim
-types.vim: src/*.[ch]
-	@echo -e "\033[1m MAKE\033[0m" types.vim
-	@exuberant-ctags --c-kinds=gstu -o- src/*.[ch] |\
-		awk 'BEGIN{echo -e("syntax keyword Type\t")}\
-			{echo -e("%s ", $$1)}END{print ""}' > $@
-
--- src/main.c
+++ src/main.c
@@ -1,26 +1,22 @@
-#include <stdio.h>
+/*
+ * Gordienko Oleg aka Gordio <admin@gordio.pp.ua>
+ * Last edited: 2013-01-21
+ * Licensed: MIT
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <time.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/sysinfo.h>
-#include <fcntl.h>
-
-#define FIFO_NAME "/tmp/dvtm-statusbar"
-#define UPDATE_SEC 1 // WARNING: UPDATE_SEC > 0
-#define NET_INTERFACE "ppp0"
 
 
-char buf[256];
-
-FILE *
+inline FILE *
 open_file(char *name)
 {
-	FILE *fd;
-	if (!(fd = fopen(name, "r"))) {
+	FILE *fd = NULL;
+
+	fd = fopen(name, "r");
+	if (!fd) {
 		fprintf(stderr, "Failed to open file '%s'", name);
 		exit(EXIT_FAILURE);
 	}
@@ -28,119 +24,59 @@ open_file(char *name)
 	return fd;
 }
 
-void
-get_net_data(FILE *fd, char *iface, uint64_t *in, uint64_t *out)
-{
-	fscanf(fd, "%*[^\n] %*[^\n] "); /* skip two lines */
-	do {
-		// read interface line
-		fscanf(fd, "%7[^:]:%lu %*u %*u %*u %*u %*u %*u %*u"
-			  "%lu %*[^\n] ", buf, in, out);
-		// check seted interface?
-		if (strcmp(buf, iface) == 0) {
-			break;
-		}
-	} while (!feof(fd));
-	// go to start and clean buffer
-	rewind(fd);
-	setbuf(fd, NULL);
-}
-
 int main(int argc, const char *argv[], const char *envp[])
 {
-	// NET
-	uint64_t net_total_in_old = -1, net_total_out_old = -1;
-	uint64_t net_total_in = 0, net_total_out = 0;
-	float net_speed_in, net_speed_out;
-	FILE *net_fd = open_file("/proc/net/dev");
-
-	// MEM
-	struct sysinfo memInfo;
-	unsigned long memFree, memTotal, memPrc;
-	unsigned long swpFree, swpTotal, swpPrc;
-
-	// TIME
-	time_t current;
-	struct tm *timeptr;
-
 	// CPU
 	long double a[4], b[4], cpuLoad;
 	FILE *cpu_fd = open_file("/proc/stat");
 
-	// FIFO
-	int fifo;
-	fifo = mkfifo(FIFO_NAME, 0777);
-	if (!access(FIFO_NAME, F_OK) == 0) {
-		fprintf(stderr, "Can't create FIFO'\n");
-		exit(EXIT_FAILURE);
-	}
+	fscanf(cpu_fd, "cpu  %Lf %Lf %Lf %Lf", &a[0], &a[1], &a[2], &a[3]);
+	rewind(cpu_fd);
+	setbuf(cpu_fd, NULL);
 
-	if ((fifo = open(FIFO_NAME, O_WRONLY)) == -1) {
-		fprintf(stderr, "Can't open FIFO\n");
-		exit(EXIT_FAILURE);
-	}
+	// Wait 1 second
+	sleep(1);
 
-	for (;;) {
-		// NET HACK: save old value
-		if (net_total_in_old == -1) {
-			get_net_data(net_fd, NET_INTERFACE, &net_total_in, &net_total_out);
-		}
+	fscanf(cpu_fd, "cpu  %Lf %Lf %Lf %Lf", &b[0], &b[1], &b[2], &b[3]);
+	rewind(cpu_fd);
+	setbuf(cpu_fd, NULL);
+
+	// find delta and find prc
+	cpuLoad = ((b[0]+b[1]+b[2]) - (a[0]+a[1]+a[2])) / ((b[0]+b[1]+b[2]+b[3]) - (a[0]+a[1]+a[2]+a[3]));
+	cpuLoad *= 100;
 
 
-		// CPU
-		fscanf(cpu_fd, "cpu  %Lf %Lf %Lf %Lf", &a[0], &a[1], &a[2], &a[3]);
-		rewind(cpu_fd);
-		setbuf(cpu_fd, NULL);
+	struct sysinfo sysInfo;
+	sysinfo(&sysInfo);
 
-		/* SET SYSTEM SLEEP */
-		sleep(UPDATE_SEC);
+	// MEM
+	unsigned int memTotal, memFree, memBuffed, memCached, memPrc;
+	FILE *ram_fd = open_file("/proc/meminfo");
 
-		fscanf(cpu_fd, "cpu  %Lf %Lf %Lf %Lf", &b[0], &b[1], &b[2], &b[3]);
-		rewind(cpu_fd);
-		setbuf(cpu_fd, NULL);
+	fscanf(ram_fd, "MemTotal: %u kB\n", &memTotal);
+	fscanf(ram_fd, "MemFree: %u kB\n", &memFree);
+	fscanf(ram_fd, "Buffers: %u kB\n", &memBuffed);
+	fscanf(ram_fd, "Cached: %u kB\n", &memCached);
 
-		cpuLoad = ((b[0]+b[1]+b[2]) - (a[0]+a[1]+a[2])) / ((b[0]+b[1]+b[2]+b[3]) - (a[0]+a[1]+a[2]+a[3]));
-		cpuLoad *= 100;
+	memPrc = (memFree + memCached) / (memTotal / 100);
 
+	// SWAP
+	unsigned long swpFree, swpTotal, swpPrc;
 
-		// MEM
-		sysinfo(&memInfo);
-		memTotal = memInfo.totalram * memInfo.mem_unit;
-		memFree = memInfo.freeram * memInfo.mem_unit;
-		memPrc = memFree / (memTotal / 100);
-		// SWAP
-		swpTotal = memInfo.totalswap * memInfo.mem_unit;
-		swpFree = memInfo.freeswap * memInfo.mem_unit;
+	swpTotal = sysInfo.totalswap * sysInfo.mem_unit;
+	swpFree = sysInfo.freeswap * sysInfo.mem_unit;
+	if (swpTotal != 0) {
 		swpPrc = swpFree / (swpTotal / 100);
 
-
-		// NET
-		net_total_in_old = net_total_in;
-		net_total_out_old = net_total_out;
-		get_net_data(net_fd, NET_INTERFACE, &net_total_in, &net_total_out);
-		net_speed_in = (float)(net_total_in - net_total_in_old) / 1024;
-		net_speed_out = (float)(net_total_out - net_total_out_old) / 1024;
-		// this is magic - sorry bad english
-		net_speed_in /= UPDATE_SEC;
-		net_speed_out /= UPDATE_SEC;
-
-
-		// TIME
-		current = time(NULL);
-		timeptr = localtime(&current);
-
-		// OUTPUT
-		sprintf(buf, "DOWN:%5.2fKb UP:%5.2fKb | "
-				"SWAP:%3.2lu%% MEM:%3.2lu%% | "
-				"CPU:%3.Lf%% | %.2d:%.2d\n",
-				net_speed_in, net_speed_out,
-				100 - swpPrc, 100 - memPrc,
-				cpuLoad, timeptr->tm_hour, timeptr->tm_min);
-		write(fifo, buf, strlen(buf));
+		printf("CPU:%3.Lf%% #[fg=green]|#[fg=default] MEM:%3.0i%% SWAP:%2.1lu%%",
+				cpuLoad, 100 - memPrc, 100 - swpPrc);
+	} else {
+		printf("CPU:%3.Lf%% #[fg=green]|#[fg=default] MEM:%3.0i%%",
+				cpuLoad, 100 - memPrc);
 	}
 
-	close(fifo);
-	fclose(net_fd);
+
+	fclose(ram_fd);
 	fclose(cpu_fd);
 	return 0;
 }
